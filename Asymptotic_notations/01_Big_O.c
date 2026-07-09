d432f35a68a2b06242adffb63be056e67c04bea1
Swarupa Shinde <swarupa8387@gmail.com>
2026-07-09 20:14:39 +0530

    Update 01_Big_O.c

--- Asymptotic_notations/01_Big_O.c
+++ Asymptotic_notations/01_Big_O.c
@@ -1,4 +1,3 @@
 // Big O notation is used to describe the upper bound of the time complexity of an algorithm. It provides a way to express how the runtime of an algorithm grows with respect to the input size.
 
 //It represents the upper bound(worst case) of the time complexity of an algorithm. It tells us the maximum time an algorithm may take.
-
